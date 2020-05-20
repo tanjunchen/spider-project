0b165d743fe0c5f694045b46fd082903b2dc0cbc
tanjunchen <2799194073@qq.com>
2020-05-20 16:02:09 +0800

    update readme.md

--- ChinaHousePrice/common/__init__.py
+++ ChinaHousePrice/common/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 11/1/2018 3:53 PM
-# @Author  : ctj
 from maoyan.common.AreaCodeDecoder import AreaCodeDecoder
 
 area_code_decoder = AreaCodeDecoder()
